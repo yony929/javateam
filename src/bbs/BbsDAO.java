88dcaae957983789631053dadff899bd7be6cada
yony929 <74482846+yony929@users.noreply.github.com>
2020-11-29 07:59:34 +0900

    Add files via upload

---
+++ src/bbs/BbsDAO.java
@@ -0,0 +1,143 @@
+package bbs;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class BbsDAO {
+
+	private Connection conn;
+	
+	private ResultSet rs;
+	
+	public BbsDAO() {
+		try {
+			String dbURL ="jdbc:mysql://localhost:3306/BBS";
+			String dbID ="root";
+			String dbPassword ="1234";
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL,dbID,dbPassword);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getDate() {
+		String SQL ="SELECT NOW()"; //현재시간을 가져옴
+		
+		try {
+			PreparedStatement pstmt =conn.prepareStatement(SQL);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getString(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ""; //데베 오류
+	}
+	
+	public int getNext() {
+		String SQL ="SELECT bbsID FROM BBS ORDER BY bbsID DESC";
+		
+		try {
+			PreparedStatement pstmt =conn.prepareStatement(SQL);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getInt(1)+1;
+			}
+			return 1; //첫번째 게시물인 경우
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; //데베 오류
+	}
+	
+	public int write(String bbsTitle, String userID, String bbsContent) {
+		String SQL ="INSERT INTO BBS VALUES (?,?,?,?,?,?)";
+		try {
+			PreparedStatement pstmt =conn.prepareStatement(SQL);
+			pstmt.setInt(1, getNext());
+			pstmt.setString(2, bbsTitle);
+			pstmt.setString(3, userID);
+			pstmt.setString(4, getDate());
+			pstmt.setString(5, bbsContent);
+			pstmt.setInt(6, 1);
+		
+			return pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; //데베 오류
+	}
+	
+	public ArrayList<Bbs> getList(int pageNumber) {
+		String SQL ="SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1 ORDER BY bbsID DESC LIMIT 10";
+		ArrayList<Bbs> list = new ArrayList<Bbs>();
+		try {
+			PreparedStatement pstmt =conn.prepareStatement(SQL);
+			pstmt.setInt(1, getNext() - (pageNumber - 1)*10);
+			
+			rs = pstmt.executeQuery();
+		    while(rs.next()) {
+		    	Bbs bbs = new Bbs();
+		    	bbs.setBbsID(rs.getInt(1));
+		    	bbs.setBbsTitle(rs.getString(2));
+		    	bbs.setUserID(rs.getString(3));
+		    	bbs.setBbsDate(rs.getString(4));
+		    	bbs.setBbsContent(rs.getString(5));
+		    	bbs.setBbsAvailable(rs.getInt(6));
+		    	list.add(bbs);	
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list; //페이지 리스트가 담겨 출력
+	}
+	
+	public boolean nextPage(int pageNumber) { //페이징처리 (게시글 10개면 1개, 11개면 2개,,)
+		String SQL ="SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1";
+		
+		try {
+			PreparedStatement pstmt =conn.prepareStatement(SQL);
+			pstmt.setInt(1, getNext() - (pageNumber - 1)*10);
+			
+			rs = pstmt.executeQuery();
+		    if (rs.next()) {
+		    	return true;
+		    	
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public Bbs getBbs(int bbsID) {
+		String SQL ="SELECT * FROM BBS WHERE bbsID = ?";
+		
+		try {
+			PreparedStatement pstmt =conn.prepareStatement(SQL);
+			pstmt.setInt(1, bbsID);
+			rs = pstmt.executeQuery();
+			
+		    if (rs.next()) {
+		    	Bbs bbs = new Bbs();
+		    	bbs.setBbsID(rs.getInt(1));
+		    	bbs.setBbsTitle(rs.getString(2));
+		    	bbs.setUserID(rs.getString(3));
+		    	bbs.setBbsDate(rs.getString(4));
+		    	bbs.setBbsContent(rs.getString(5));
+		    	bbs.setBbsAvailable(rs.getInt(6));
+		    	return bbs;
+		    	
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+}
+
